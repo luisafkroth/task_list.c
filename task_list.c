0eb46dd2a84dbb13131dbb9f800645cbee4422bb
luisafkroth <luisafkroth@gmail.com>
2024-06-30 23:00:19 -0300

    almost finished

--- task_list.c
+++ task_list.c
@@ -1,70 +1,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "task_list.h"
 
-void init_list(list *list)
-{
-	list->head=NULL;
-	list->tail=NULL;
+void initializeRandom() { /// Função para iniciar o gerador de números aleatórios
+    srand(time(NULL));
 }
 
-void addTask (list *list, char *description, int time)
-{
-    task *aux;
-    aux = (task *)malloc(sizeof(task));
-    strcpy(aux->description, description);
-    aux->time = time;   
-    aux->status = ATIVA;
-    aux->next = NULL;
-    if (list->head==NULL) {
-        aux->id = 1;
-        aux->prev = NULL;
-        list->head = aux;
-        list->tail = aux;
-        return;
+int generateId() {  /// Função para gerar um ID aleatório
+    int id = (rand() % 100);
+    return id;
+}
+
+int idExists( taskNode *root, int id) {  /// Função para checar se um ID existe
+    if (root == NULL) {
+        return 0; // False
     }
-    aux->id = list->tail->id+1;
-    aux->prev = list->tail;
-    list->tail->next = aux;
-    list->tail = aux;
-    return;
+    if (root->id == id) {
+        return 1; // True
+    } else if (id < root->id) {
+        return idExists(root->left, id);
+    } else {
+        return idExists(root->right, id);
+    }
 }
 
-task* findById(list *list, int id) {
-    task *aux;
-    if (list == NULL || list->head == NULL) {
-        printf("A lista está vazia!\n");
+
+taskNode* findById(taskNode *root, int id) {
+    if (root == NULL) {
         return NULL;
     }
-    for (aux=list->head; aux->next!=NULL; aux=aux->next) {
-        if (aux->id==id) {
-            return aux;
-        }
-    }
-    printf("ID %d não encontrado! \n", id);
-    return NULL;
-}
-
-void showTasks(list *list) {
-
-}
-void concludeTask(list *list, int id) {
-    task *aux = findById(list, id);
-    aux->status = CONCLUIDA;
-}
-
-void excludeTask(list *list, int id) {
-    task *aux = findById(list, id);
-    if (aux==list->head) {
-        list->head = list->head->next;
-        list->head->prev = NULL;
-    } if (aux==list->tail) {
-        list->tail = list->tail->prev;
-        list->tail->next = NULL;
+    if (id == root->id) {
+        return root;
+    } else if (id < root->id) {
+        return findById(root->left, id);
     } else {
-        aux->prev->next = aux->next;
-        aux->next->prev = aux->prev;
+        return findById(root->right, id);
     }
-    free(aux);
+}
+
+taskNode* createTask(taskNode *root, char *description, int time) {  ///Função para criar uma tarefa
+    taskNode *newTask = (taskNode *)malloc(sizeof(taskNode));
+    int id = generateId();
+    while (idExists(root, id)) {             /// Loop para conferir se um ID existe, e, se sim, criar um novo ID
+        id = generateId();
+    }
+    newTask->id = id;
+    strncpy(newTask->description, description, sizeof(newTask->description) - 1);
+    newTask->description[sizeof(newTask->description) - 1] = '\0'; 
+    newTask->time = time;
+    newTask->status = 1;         ///Tarefa ativa = 1 e concluída = 0
+    newTask->left = NULL;
+    newTask->right = NULL;
+    return newTask;
+}
+
+taskNode* addTask(taskNode *root, taskNode *newTask)  /// Função para adicionar uma tarefa
+{
+    if (root==NULL) {
+        return newTask;
+    }
+    printf("%p\n", root);
+    if (newTask->id < root->id) {
+        root->left = addTask(root->left, newTask);
+    } else {
+        root->right = addTask(root->right, newTask);
+    }
+    return root; 
+}
+
+taskNode* addTaskTime(taskNode *root, taskNode *newTask)  /// Função para adicionar uma tarefa na árvore baseada no tempo
+{
+    if (root==NULL) {
+        return newTask;
+    }
+    if (newTask->time < root->time) {
+        root->left = addTaskTime(root->left, newTask);
+    } else {
+        root->right = addTaskTime(root->right, newTask);
+    }
+    return root; 
+}
+
+void printTask(taskNode *task) {     /// Função para padronizar impressão de tarefas
+    printf("-------------------------------------\n");
+    printf("ID: %d \n", task->id);
+    printf("Descricao: %s \n", task->description);
+    printf("Tempo: %d horas \n", task->time);
+     if (task->status == 1) {
+        printf("Status: ATIVA\n");
+    } else {
+        printf("Status: CONCLUIDA\n"); 
+    }
+    printf("-------------------------------------\n");
+}
+
+void showActiveTasks(taskNode *root) {    ///Função para imprimir apenas as tarefas ativas
+    if (root==NULL) return;
+    showActiveTasks(root->left);
+    if (root->status == 1) {
+        printTask(root);
+    }
+    showActiveTasks(root->right);
+}
+
+void showCompletedTasks(taskNode *root) {    ///Função para imprimir apenas as tarefas concluídas
+    if (root==NULL) return;
+    showCompletedTasks(root->left);
+    if (root->status == 0) {
+        printTask(root);
+    }
+    showCompletedTasks(root->right);
+}
+
+void concludeTask(taskNode *root, int id) {        ///Função para concluir uma tarefa
+    taskNode *task = findById(root, id);
+    if (task != NULL) {
+        task->status = 0;
+    }
+}
+
+taskNode* findMin(taskNode *root)          /// Função para encontrar o nó com menor valor na subárvore direita
+{
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+taskNode* excludeTask(taskNode *root, int id)  /// Função para excluir uma tarefa
+{
+    if (root == NULL) return root;
+    if (id < root->id) {
+        root->left = excludeTask(root->left, id);
+    } else if (id > root->id) {
+        root->right = excludeTask(root->right, id);
+    } else {
+        if (root->left == NULL) {
+            taskNode *temp = root->right;
+            free(root);
+            return temp;        
+        } else if (root->right == NULL) {
+            taskNode *temp = root->left;
+            free(root);
+            return temp;
+        }
+        taskNode *temp = findMin(root->right);
+        root->id = temp->id;
+        strncpy(root->description, temp->description, sizeof(root->description) - 1);
+        root->description[sizeof(root->description) - 1] = '\0';
+        root->time = temp->time;
+        root->status = temp->status;
+        root->right = excludeTask(root->right, temp->id);
+    }
+    return root;
 }
--- task_list_main.c
+++ task_list_main.c
@@ -1,76 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "task_list.h"
 
 int main() {
 
-    int choice;
-    list *task_list;
-    init_list(task_list);
+     initializeRandom();
+    
+    taskNode *taskTree = NULL;  //Árvore principal
+    taskNode *taskTreeTime = NULL; //Árvore criada com base no tempo
 
     while(1) {
 
-        printf("\nMenu de Opções:\n");
+        int choice;
+        printf("\nMenu de Opcoes:\n\n");
         printf("1. Adicionar tarefa \n");
         printf("2. Visualizar tarefas \n");
         printf("3. Concluir tarefas \n");
         printf("4. Excluir tarefas \n");
         printf("5. Sair \n");
-        printf("Escolha uma opção: ");
+        printf("\nEscolha uma opcao: ");
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1:
-            char *description[300];
-            int time;
+            case 1: 
+            {
+                char description[300];
+                int time;
 
-            printf("Digite a descrição da tarefa a ser adicionada.");
-            scanf("%s", description);
-            printf("Digite o tempo (em horas) para realizar esta tarefa.");
-            scanf("%d", &time);
-            addTask(task_list, description,time);
-            break;
-
-        case 2:
-            int visualization;
-            while (1) {
-                printf("Você deseja visualizar: \n");
+                printf("\nDigite a descricao da tarefa a ser adicionada: ");
+                scanf(" %[^\t\n]s", description);
+                printf("Digite o tempo (em horas) para realizar esta tarefa: ");
+                scanf("%d", &time);
+                taskNode *newTask = createTask(taskTree, description, time);
+                taskNode *newTask2 = (taskNode *)malloc(sizeof(taskNode));
+                memcpy(newTask2, newTask, sizeof *newTask);
+                taskTree = addTask(taskTree, newTask);  //Adiciona na árvore principal
+                taskTreeTime = addTaskTime(taskTreeTime, newTask2);  //Adiciona na árvore baseada no tempo
+                break;
+            }
+            case 2: 
+            {
+                int visualization;
+                printf("\nVoce deseja visualizar: \n\n");
                 printf("1.Todas as tarefas \n");
                 printf("2. Tarefas ativas \n");
                 printf("3. Tarefas concluidas \n");
-                printf("Digite a opção escolhida: ");
-                scanf("%d", visualization);
+                printf("\nDigite a opcao escolhida: ");
+                scanf("%d", &visualization);
 
-                switch (choice) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    default:
-                         printf("Opção inválida! Tente novamente.\n");
+                if (visualization == 1) {
+                    showActiveTasks(taskTreeTime);        ///Imprime primeiro todas as tarefas ativas
+                    showCompletedTasks(taskTreeTime);     ///Depois imprime as concluídas
+                    break;
+                } else if (visualization == 2) {
+                    showActiveTasks(taskTreeTime);
+                    break;
+                } else if (visualization == 3) {
+                    showCompletedTasks(taskTreeTime);
+                    break;
+                } else {
+                    printf("Opcao invalida! Tente novamente.\n");
                 }
+                break;
             }
-            break;
-        case 3:
-            int id;
-            printf("Digite o ID da tarefa que você deseja concluir. \n");
-            scanf("%d", &id);
-            concludeTask(task_list, id);
-            break;
-        case 4:
-            int id;
-            printf("Digite o ID da tarefa que você deseja excluir. \n");
-            scanf("%d", &id);
-            excludeTask(task_list, id);
-            break;
-        case 5:
-            printf("Saindo do programa...\n");
-            exit(0);
-        default:
-            printf("Opção inválida! Tente novamente.\n");
+            case 3:
+            {
+                int id;
+                printf("Digite o ID da tarefa que voce deseja concluir. \n");
+                scanf("%d", &id);
+                if (idExists(taskTree, id) == 1) {
+                    concludeTask(taskTree, id);       //Conclui a tarefa na árvore principal
+                    concludeTask(taskTreeTime, id);   //Conclui a tarefa na árvore baseada no tempo
+                    printf("Tarefa de ID %d concluida. \n", id);
+                } else {
+                    printf("Nao existe nenhuma tarefa com o ID %d \n", id);
+                }
+                break;
+            }
+            case 4:
+            {
+                int id;
+                printf("Digite o ID da tarefa que voce deseja excluir. \n");
+                scanf("%d", &id);
+                if (idExists(taskTree, id)) {
+                    taskTree = excludeTask(taskTree, id);      //Exclui a tarefa da árvore principal
+                    taskTreeTime = excludeTask(taskTreeTime, id);  //Exclui a tarefa da árvore baseada no tempo
+                    printf("Tarefa de ID %d excluida. \n", id);
+                } else {
+                    printf("Nao existe nenhuma tarefa com o ID %d \n", id);
+                }
+                break;
+            }
+            case 5:
+            {
+                printf("Saindo do programa...\n");
+                exit(0);
+            }
+            default:
+            {
+                printf("Opcao invalida! Tente novamente.\n");
+            }
         }
     }
 }
